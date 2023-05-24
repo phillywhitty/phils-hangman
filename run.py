8e5f26dc3cf730d4802806b296df9e6c0aa61e89
phillywhitty <philipwhitty92@yahoo.ie>
2023-05-24 18:09:59 +0000

    add game_intro and main_functions

--- run.py
+++ run.py
@@ -1,5 +1,34 @@
 import random
 from words import list_of_words
 
-print(list_of_words)
+
+
+def game_intro():
+      print("""
+    
+ __ __   ____  ____    ____  ___ ___   ____  ____  
+|  |  | /    ||    \  /    ||   |   | /    ||    \ 
+|  |  ||  o  ||  _  ||   __|| _   _ ||  o  ||  _  |
+|  _  ||     ||  |  ||  |  ||  \_/  ||     ||  |  |
+|  |  ||  _  ||  |  ||  |_ ||   |   ||  _  ||  |  |
+|  |  ||  |  ||  |  ||     ||   |   ||  |  ||  |  |
+|__|__||__|__||__|__||___,_||___|___||__|__||__|__|
+                                                   
+    """)
+
+
+
+
+
+
+def main():
+    """
+    Runs the game functions
+    """
+    game_intro()
+
+main()
+
+
+
 
